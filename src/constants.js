4365b113c35d96179ebb9126074fa5742cb45922
a602484 <bob.macneal@target.com>
2019-02-22 09:01:11 -0600

    Add users resource

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,10 @@
 export const JSON_PLACEHOLDER_API_URL = 'https://jsonplaceholder.typicode.com'
+export const ID_FIELD = 'id'
 export const FETCH_LIMIT = 50
 export const RESOURCE_TYPE = {
   PHOTOS: 'photos',
   POSTS: 'posts',
   TODOS: 'todos',
+  USERS: 'users',
 }
 
