ea5992644c58aedcad4abc675cab2b836c746b4d
a602484 <bob.macneal@target.com>
2019-02-27 14:25:02 -0600

    Simplify service

--- src/services/resourcesService.js
+++ src/services/resourcesService.js
@@ -2,18 +2,17 @@ import {FETCH_LIMIT, JSON_PLACEHOLDER_API_URL} from "../constants"
 import {useEffect, useState} from 'react'
 import axios from 'axios'
 
-export const getResources = resource => {
+export function getResources (resource) {
   const [resources, setResources] = useState([])
-  // useEffect only runs async function when the resource prop changes (similar to componentDidUpdate
-  // lifecycle method where we used to compare a prop to a prevProps.prop before invoking a function)
-  useEffect(
-    () => {
-      (async resource => {
-        const response = await axios.get(`${JSON_PLACEHOLDER_API_URL}/${resource}`)
-        setResources(response.data)
-      })(resource) // syntax here is to define async function "resource" then invoke it. That is: (define)(invoke)
-    },
-    [resource] // useEffect needs this to determine if function should be run (won't run if [resource] hasn't changed).
-  )
+
+  async function getData() {
+    const response = await axios.get(`${JSON_PLACEHOLDER_API_URL}/${resource}`)
+    setResources(response.data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [resource])
+
   return resources.slice(0, FETCH_LIMIT) // limit to FETCH_LIMIT records
 }
