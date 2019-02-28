99f96a1e34c9f3f40b314b0f47422ccff5057dc6
a602484 <bob.macneal@target.com>
2019-02-28 08:46:53 -0600

    Modify formatting

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,6 @@
 export const JSON_PLACEHOLDER_API_URL = 'https://jsonplaceholder.typicode.com'
 export const ID_FIELD = 'id'
-export const FETCH_LIMIT = 50
-export const RESOURCE_TYPE = {
+export const resourceCategory = {
   PHOTOS: 'photos',
   POSTS: 'posts',
   TODOS: 'todos',
